eb15e9f78433e02fd007d2a829bef2c8759d6b75
admin <admin@3440-MacBook-Pro.local>
2021-12-26 17:12:27 +0500

    perf(devops): workout state pattern

--- patterns/__test__/20_State.test.js
+++ patterns/__test__/20_State.test.js
@@ -1,5 +1,63 @@
+/**
+ * @description The State pattern provides state-specific logic to a limited set of objects in which each object represents a particular state.
+ * @link https://www.dofactory.com/javascript/design-patterns/state
+ * @test yarn jest patterns/__test__/20_State.test.js
+ */
+
+// TrafficLight
+// CreateStore
+// count
+// currentState
+// change
+// start
+
+// Red
+// Yellow
+// Green
+// light
+// go
+
 describe('Behavioral Patterns', () => {
-  it("--  Pattern: State  Alter an object's behavior when its state changes", () => {
+  it("--  2020=12-26 Pattern: State  Alter an object's behavior when its state changes", () => {
+    class CreateStore {
+      constructor(props) {
+        this.state = [props]
+      }
+
+      state = []
+
+      dispatch(lightObj) {
+        this.state = [...this.state, lightObj]
+      }
+
+      getState() {
+        return this.state.slice(-1)[0]
+      }
+
+      getStateHistory() {
+        return this.state
+      }
+    }
+
+    const store = new CreateStore({ light: 'red' })
+    store.dispatch({ light: 'green' })
+
+    // console.info('state 2', {
+    //   stateHistory: store.getStateHistory(),
+    //   getState: store.getState(),
+    // })
+
+    const expectStateHistory = [
+      { light: 'red' },
+      { light: 'yellow' },
+      { light: 'green' },
+    ]
+    const expectState = { light: 'green' }
+    expect(store.getStateHistory()).toEqual(expectStateHistory)
+    expect(store.getState()).toEqual(expectState)
+  })
+
+  it("--  2020=11-01 Pattern: State  Alter an object's behavior when its state changes", () => {
     function CreateStore(initialState) {
       this.state = [initialState]
     }
@@ -36,15 +94,3 @@ describe('Behavioral Patterns', () => {
     expect(store.getState()).toEqual(expectState)
   })
 })
-
-// TrafficLight
-// count
-// currentState
-// change
-// start
-
-// Red
-// Yellow
-// Green
-// light
-// go
