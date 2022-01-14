4cbdba26367c9a33ec4b28fb7418a1d39b1d6925
admin <admin@3440-MacBook-Pro.local>
2022-01-14 16:36:50 +0500

    perf(devops): workout isInteger, isPrime functions

--- algorithms/__test__/isPrime.test.js
+++ algorithms/__test__/isPrime.test.js
@@ -5,41 +5,88 @@
  */
 
 describe('Algoritms', () => {
-  it('--  isPrime', () => {
-    let arr = [
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      11,
-      12,
-      13,
-      14,
-      15,
-      16,
-      17,
-      18,
-      19,
+  it('-- isPrime', () => {
+    const isPrime = val => {
+      if (val < 1) return false
+      if (val === 1) return true
+
+      let output2 = true
+      let iter = val - 1
+      while (iter !== 1) {
+        output2 = (val / iter) % Math.floor(val / iter) !== 0
+        // console.info('isPrime.test [14]', {
+        //   iter,
+        //   'val / iter': val / iter,
+        //   'Math.floor(val / iter)': Math.floor(val / iter),
+        //   '(val / iter) % Math.floor(val / iter)':
+        //     (val / iter) % Math.floor(val / iter),
+        //   output,
+        //   val,
+        // })
+        if (output2 === false) break
+        iter -= 1
+      }
+
+      return output2
+    }
+
+    const input = [
+      -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
       20,
     ]
 
-    // arr = [5]
+    // -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+
+    const expected = [
+      { item: -1, isPrimeTF: false },
+      { item: 0, isPrimeTF: false },
+      { item: 1, isPrimeTF: true },
+      { item: 2, isPrimeTF: true },
+      { item: 3, isPrimeTF: true },
+      { item: 4, isPrimeTF: false },
+      { item: 5, isPrimeTF: true },
+      { item: 6, isPrimeTF: false },
+      { item: 7, isPrimeTF: true },
+      { item: 8, isPrimeTF: false },
+      { item: 9, isPrimeTF: false },
+      { item: 10, isPrimeTF: false },
+      { item: 11, isPrimeTF: true },
+      { item: 12, isPrimeTF: false },
+      { item: 13, isPrimeTF: true },
+      { item: 14, isPrimeTF: false },
+      { item: 15, isPrimeTF: false },
+      { item: 16, isPrimeTF: false },
+      { item: 17, isPrimeTF: true },
+      { item: 18, isPrimeTF: false },
+      { item: 19, isPrimeTF: true },
+      { item: 20, isPrimeTF: false },
+    ]
+
+    let output = input.map(item => ({ item, isPrimeTF: isPrime(item) }))
+
+    expect(output).toEqual(expected)
+  })
+
+  it('--  isPrime 2021', () => {
+    let arr = [
+      -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+      20,
+    ]
 
     const isPrime = num => {
+      if (num < 1) return false
+      if (num === 1) return true
+
       const range = [...Array(num).keys()]
-      let isPrime = true
+      let output = true
       range.forEach(item => {
-        if (item & (item !== 1) & (num % item === 0)) isPrime = false
+        const condition =
+          output === true && !!item && item !== 1 && num % item === 0
+
+        output = condition ? false : output
       })
 
-      return isPrime
-      // console.info('[34]', { range })
+      return output
     }
 
     let arrOut = []
@@ -48,16 +95,17 @@ describe('Algoritms', () => {
       arrOut = [...arrOut, { item, isPrimeTF }]
     })
 
-    // console.info('[51]', { arrOut })
     const expected = [
+      { item: -1, isPrimeTF: false },
+      { item: 0, isPrimeTF: false },
       { item: 1, isPrimeTF: true },
       { item: 2, isPrimeTF: true },
       { item: 3, isPrimeTF: true },
-      { item: 4, isPrimeTF: true },
+      { item: 4, isPrimeTF: false },
       { item: 5, isPrimeTF: true },
       { item: 6, isPrimeTF: false },
       { item: 7, isPrimeTF: true },
-      { item: 8, isPrimeTF: true },
+      { item: 8, isPrimeTF: false },
       { item: 9, isPrimeTF: false },
       { item: 10, isPrimeTF: false },
       { item: 11, isPrimeTF: true },
@@ -65,7 +113,7 @@ describe('Algoritms', () => {
       { item: 13, isPrimeTF: true },
       { item: 14, isPrimeTF: false },
       { item: 15, isPrimeTF: false },
-      { item: 16, isPrimeTF: true },
+      { item: 16, isPrimeTF: false },
       { item: 17, isPrimeTF: true },
       { item: 18, isPrimeTF: false },
       { item: 19, isPrimeTF: true },
